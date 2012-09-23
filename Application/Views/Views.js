867a71030d514175e8ce6f575527e96c79b24a97
Yuriy Kurylenko <yuriy@yuriy-Latitude-E6420.(none)>
2012-09-24 01:19:56 +0300

    Leaving only framework files and its dependencies

--- Application/Views/Views.js
+++
@@ -1,7 +0,0 @@
-App.View.DeviceTargetingPanel = Backbone.View.extend({
-	render: function() {
-		var formTag = '<form id="deviceTargetingForm" wId="deviceTargetingForm" wtype="form" template="DeviceTargeting" model="App.Model.DeviceTargeting">';
-		$(this.el).append(formTag).form();
-		return this;
-	}
-});
